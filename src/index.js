1c3f84f0a6a53503ab37ca209c0e7620eafa7294
Siarhei Sudakov <s3gaddtv@gmail.com>
2020-02-20 17:12:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        let minimum = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (minimum > array[i]) minimum = array[i];
+        }
+        return minimum;        
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+        return 0;
+    } else {
+        let maximum = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (maximum < array[i]) maximum = array[i];
+        }
+        return maximum;        
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+        return 0;
+  }else {
+      let average = array[0];
+    for (let i = 1; i < array.length; i++) {
+        average += array[i];
+    }
+    return average / array.length;
+  }
+    
 }
